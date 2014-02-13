fadba781a80471969b0394b1b04e63f57c095362
GeertJohan <gjr19912@gmail.com>
2014-02-13 21:51:34 +0100

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package ango is a tool that generates a protocol for communication between Go and AngularJS over http/websockets.
+// For information about this tool, visit github.com/GeertJohan/ango
+package ango
